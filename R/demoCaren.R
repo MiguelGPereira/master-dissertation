3212dffec9942ae00d523985c56b15a853649838
MiguelGPereira <miguelgopereira@gmail.com>
2017-09-13 01:56:17 +0100

    fixed discretization w/cutpoints on TEST data. tau being calculated

--- R/demoCaren.R
+++ R/demoCaren.R
@@ -7,23 +7,26 @@ source("edira/edira.R")
 
 # read DATA
 ## PT
-#out <- read.csv("../data/out_german_based.csv", sep = ";", stringsAsFactors=FALSE, fileEncoding="latin1")
+out <- read.csv("../data/out_german_based_charfix.csv", sep = ";", stringsAsFactors=FALSE, fileEncoding="latin1")
 ## GET
-out <- read.csv("../data/out_german.csv", sep = ";", stringsAsFactors=FALSE, fileEncoding="latin1")
+#out <- read.csv("../data/out_german.csv", sep = ";", stringsAsFactors=FALSE, fileEncoding="latin1")
 
 # transform DATA
 ## select rows of relevant parties
 ### PT
-#rankings <- out[,c(3,4,11,15,29)]
+rankings <- out[,c(3,4,11,15,29)]
 ### GER
-rankings <- out[,c(33:37)]
+#rankings <- out[,c(33:37)]
 ## split in independent variables (x) and votes (y)
 ### PT
-#y <- t(apply(-rankings, 1, rank))
-#x <- out[,-c(0:33)]
+y <- t(apply(-rankings, 1, rank))
+x <- out[,-c(0:33)]
 ### GER
-y <- t(apply(rankings, 1, rank))
-x <- out[,c(4:32)]
+#y <- t(apply(rankings, 1, rank))
+#x <- out[,c(4:32)]
+## transform dataframes in matrixes
+x <- as.matrix(x)
+y <- as.matrix(y)
 ## sample size percentage for TRAIN
 smp_size <- floor(0.75 * nrow(x))
 ## set the seed to make the partition reproductible
@@ -42,13 +45,32 @@ xd = DISC$Disc.data
 # mine LR rules
 rulz <- aflrC7(xd, y, msup = 1, mconf = 70, mlift = 0, mimp = 0, theta = 0, Xmx = "2000M")
 
+getNonZeroIndexes <- function(cutp) {
+  out <- c()
+  outIndex <- 1
+  zeroRow <- list(numeric(0))
+  for (i in 1:length(cutp)) {
+    l <- cutp[i]
+    if (!identical(l, zeroRow)) {
+      out[outIndex] <- i
+      outIndex <- outIndex + 1
+    }
+  }
+  return(out)
+}
+
+validIndexes <- getNonZeroIndexes(DISC$cutp)
+#xs <- xs[, validIndexes]
+#DISC$cutp = DISC$cutp[validIndexes]
+
 # discretize TEST 
-xsd <- sapply(1:nrow(xs), function(j){
+xsd <- sapply(1:ncol(xs), function(j){
   findInterval(xs[,j], c(-Inf, DISC$cutp[[j]], Inf))
 })
 
 # predict LRARs
-yp <- crank(rulz, xsd, ys, std, m2, mt)
+yp <- crank(rulz, xsd, ys, std, m2, 1)#mt)
 
 # evaluate predictions
 tau <- mean(sapply(1:nrow(ys), function(j) cor(ys[j,], yp[,j], method="kendall")))
+tau
